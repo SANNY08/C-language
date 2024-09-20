66c251f4e5441e22689fa906f65710bf0586df63
Sanny Kumar <154778296+SANNY08@users.noreply.github.com>
2024-09-20 17:41:22 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float l,b;//l = length, b= breadth
+    printf("Enter length:");
+    scanf("%f",&l);
+    printf("Enter Breadth :");
+    scanf("%f",&b);
+    printf("Area of Rectangle is %f:",l*b);
+    getch();
+}

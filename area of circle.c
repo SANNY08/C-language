d9f85bfe39ba7e08cf4710f6e1dd66762ef644ab
Sanny Kumar <154778296+SANNY08@users.noreply.github.com>
2024-09-20 17:40:46 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+void main()
+{
+    float radius;
+    printf("Enter radius :");
+    scanf("%f",&radius);
+    printf("Area of circle is :%f",pi*radius*radius);
+    getch();
+
+}

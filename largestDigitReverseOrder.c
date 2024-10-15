f6edc594ff75ebe2a4e9f236e4e4bf3c401d5c90
Sanny Kumar <154778296+SANNY08@users.noreply.github.com>
2024-10-15 15:53:26 +0530

    Add files via upload

---
+++ largestDigitReverseOrder.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,digit,largest=0;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    
+    do{
+        digit = n % 10;
+        
+        if(digit > largest)
+        {
+            largest = digit;
+        }
+
+        n = n/10;
+    }
+    while( n !=0);
+    printf("The largest number is %d\n ",largest);
+    return 0;
+}
